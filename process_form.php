1f31882b578e4a5874b90828438afa2b04f3e6af
necmibatuhan <124209749+necmibatuhan@users.noreply.github.com>
2024-01-04 16:56:42 +0300

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,26 +1,26 @@
 <?php
 include("connect.php");
 
-if(isset($_POST["yt0"])){
-  $name = $_POST["name"];
-  $mail = $_POST["email"];
-  $habit = $_POST["habit"];
+if ($_POST) {
+    $name = $_POST["name"];
+    $mail = $_POST["email"];
+    $habit = $_POST["habit"];
 
+    $sql = "INSERT INTO dbo.registration (name, email, habit) VALUES (:name, :email, :habit)";
 
+    try {
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':email', $mail);
+        $stmt->bindParam(':habit', $habit);
 
+        $calistirekle = $stmt->execute();
 
-$sql = "INSERT INTO dbo.registration (name, email, habit) VALUES (:name, :email, :habit)";
-
-$stmt = $conn->prepare($sql);
-$stmt->bindParam(':name', $name);
-$stmt->bindParam(':email', $mail);
-$stmt->bindParam(':habit', $habit);
-
-
-$calistirekle = $stmt->execute();
-}
-if ($calistirekle) {
-  echo '<script>alert("Bağlantı başarıyla kuruldu. Projemize destek olduğunuz için teşekkür ederiz.");</script>';
+        if ($calistirekle) {
+            echo '<script>alert("Bağlantı başarıyla kuruldu. Projemize destek olduğunuz için teşekkür ederiz.");</script>';
+        }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
 }
 ?>
-
