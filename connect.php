a7317271c86a3720f88df95640575a163fa8cb3b
necmibatuhan <124209749+necmibatuhan@users.noreply.github.com>
2024-01-04 16:00:43 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "batu", "pwd" => "{your_password_here}", "Database" => "registration", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "batu", "pwd" => "{Kh56662017924}", "Database" => "registration", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:it321nba.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
