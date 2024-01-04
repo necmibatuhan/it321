75141158f874c7bd455b85f53ba672d78672b927
necmibatuhan <124209749+necmibatuhan@users.noreply.github.com>
2024-01-04 16:15:58 +0300

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -2,17 +2,17 @@ include("connect.php");
 
 if(isset($_POST["yt0"])){
   $name = $_POST["name"];
-  $mail = $_POST["mail"];
+  $mail = $_POST["email"];
   $habit = $_POST["habit"];
 
 
 
 
-$sql = "INSERT INTO dbo.registration (name, email, habit) VALUES (:name, :mail, :habit)";
+$sql = "INSERT INTO dbo.registration (name, email, habit) VALUES (:name, :email, :habit)";
 
 $stmt = $conn->prepare($sql);
 $stmt->bindParam(':name', $name);
-$stmt->bindParam(':mail', $mail);
+$stmt->bindParam(':email', $mail);
 $stmt->bindParam(':habit', $habit);
 
 
