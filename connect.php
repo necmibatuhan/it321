d42c06c7096bee47398ab1c8c036173dedf6362d
necmibatuhan <124209749+necmibatuhan@users.noreply.github.com>
2024-01-04 15:59:16 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,14 +1,6 @@
-
 <?php
-$serverName = "tr1.azurewebsites.net";
-$connectionOptions = array(
-    "Database" => "registration",
-    "Uid" => "batu",
-    "PWD" => "Kh56662017924"
-);
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-
-if ($conn === false) {
-    die(print_r(sqlsrv_errors(), true));
-}
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "batu", "pwd" => "{your_password_here}", "Database" => "registration", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:it321nba.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
